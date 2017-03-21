4f8af002315aa5039f992419acd35d90a836c1c6
Amovi <anthony.movizzo@outlook.com>
2017-03-21 14:39:56 -0400

    Updated from Brython Server: 3/21/2017 2:39:56 PM

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,5 @@
 import math
-list = input (int("How many numbers should we print?"))
-fizz = input (int("For numbers of what multiple should we print 'fizz'?"))
-buzz = input (int("For numbers of what multiple should we print 'buzz'"))
+list = int (input("How many numbers should we print?"))
+fizz = int (input("For numbers of what multiple should we print 'fizz'?"))
+buzz = int (input("For numbers of what multiple should we print 'buzz'"))
+fizzbuzz = for fizz + buzz
