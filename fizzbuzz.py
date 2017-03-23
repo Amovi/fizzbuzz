17ad4161e946ba838bbc97141c07d1b81559c8b4
Amovi <anthony.movizzo@outlook.com>
2017-03-23 14:21:44 -0400

    Updated from Brython Server: 3/23/2017 2:21:43 PM

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,7 +2,7 @@ import math
 list = int (input("How many numbers should we print?"))
 fizz = int (input("For numbers of what multiple should we print 'fizz'?"))
 buzz = int (input("For numbers of what multiple should we print 'buzz'"))
-fizzbuzz = (fizz  buzz):
+fizzbuzz = (fizz + buzz):
     print "numbers that are multiples of both numbers"
 for i in xrange(1, 101):
     if i % fizzbuzz == 0:
